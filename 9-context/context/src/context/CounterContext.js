e1b191ec4514e440bd168cc960566a11efbd017c
vitorvitor07 <vitorsilva_53@hotmail.com>
2024-02-20 23:29:41 -0300

    context api added

---
+++ 9-context/context/src/context/CounterContext.js
@@ -0,0 +1,9 @@
+// 1 - criar contexto
+import { createContext, useState } from "react";
+
+export default CounterContext = createContext();
+
+// 2 - criar provider
+export const CounterContextProvider = ({ children }) => {
+  
+}
